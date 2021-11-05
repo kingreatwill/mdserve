a5be4b4e3baeda5f92ec176a7b23aa314cd30cfa
kingreatwill <350840291@qq.com>
2021-11-05 16:53:10 +0800

    up

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-docker build -t mdserve:1.4 -f ./dockerfile .
+docker build --no-cache -t mdserve:1.4 -f ./dockerfile .
 docker stop mdserve
 docker rm mdserve
 docker run -itd -p 8001:8080 -v /root/github/open:/usr/share/mdserve --name mdserve mdserve:1.4
