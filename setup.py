fd101320245d892769a34fb8f2fce399e6b564a7
kingreatwill <350840291@qq.com>
2021-11-06 16:46:08 +0800

    1.4.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 NAME = "mdserve"
-VERSION = "1.4.4"
+VERSION = "1.4.5"
 requirements = [
     n.strip() for n in read('requirements.txt').split('\n') if n.strip()
 ]
@@ -38,5 +38,5 @@ setup(
 python setup.py check
 python setup.py sdist bdist_wheel
 # python setup.py install
-twine upload dist/mdserve-1.4.4*
+twine upload dist/mdserve-1.4.5*
 """
